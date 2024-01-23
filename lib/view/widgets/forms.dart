7ea654be1ad2aae99bb85819779a33c312322b27
Chandra <ardhyachandrax@gmail.com>
2024-01-23 16:01:08 +0700

    init custom form field

---
+++ lib/view/widgets/forms.dart
@@ -0,0 +1,41 @@
+import 'package:e_wallet/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class CustomFormField extends StatelessWidget {
+  const CustomFormField({
+    super.key,
+    required this.title,
+    this.obscureText = false,
+  });
+
+  final String title;
+  final bool obscureText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: blackTextStyle.copyWith(
+            fontSize: 14,
+            fontWeight: medium,
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        TextFormField(
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.all(12),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(14),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
