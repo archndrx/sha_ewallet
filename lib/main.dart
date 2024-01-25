219c4a37876148787fe55dafcd47017fc78bcf77
Chandra <ardhyachandrax@gmail.com>
2024-01-25 20:10:09 +0700

    refactor theme & init new route

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:e_wallet/shared/theme.dart';
 import 'package:e_wallet/view/pages/home_page.dart';
 import 'package:e_wallet/view/pages/onboarding_page.dart';
-import 'package:e_wallet/view/pages/profile_page.dart';
+import 'package:e_wallet/view/pages/pin_page.dart';
+import 'package:e_wallet/view/pages/profile_pages/profile_edit_pin_page.dart';
+import 'package:e_wallet/view/pages/profile_pages/edit_profile_page.dart';
+import 'package:e_wallet/view/pages/profile_pages/profile_page.dart';
+import 'package:e_wallet/view/pages/profile_pages/profile_success_page.dart';
 import 'package:e_wallet/view/pages/sign_in_pages/sign_in_page.dart';
 import 'package:e_wallet/view/pages/sign_up_pages/sign_up_page.dart';
 import 'package:e_wallet/view/pages/sign_up_pages/sign_up_set_id_page.dart';
@@ -22,6 +26,18 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         scaffoldBackgroundColor: lightBackgroundColor,
+        appBarTheme: AppBarTheme(
+          elevation: 0,
+          centerTitle: true,
+          backgroundColor: lightBackgroundColor,
+          iconTheme: IconThemeData(
+            color: blackColor,
+          ),
+          titleTextStyle: blackTextStyle.copyWith(
+            fontWeight: semibold,
+            fontSize: 20,
+          ),
+        ),
       ),
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
@@ -35,6 +51,10 @@ class MainApp extends StatelessWidget {
         '/sign-up-success': (context) => const SignUpSuccessPage(),
         '/home': (context) => const HomePage(),
         '/profile': (context) => const ProfilePage(),
+        '/pin': (context) => const PinPage(),
+        '/profile-edit': (context) => const EditProfilePage(),
+        '/profile-pin-edit': (context) => const ProfileEditPinPage(),
+        '/profile-edit-success': (context) => const ProfileSuccessPage(),
       },
     );
   }
