8064ce6c7d71285b81fabaa9229acded2a18b0cc
Chandra <ardhyachandrax@gmail.com>
2024-01-25 20:10:37 +0700

    slicing edit profile page

---
+++ lib/view/pages/profile_pages/edit_profile_page.dart
@@ -0,0 +1,82 @@
+import 'package:e_wallet/shared/theme.dart';
+import 'package:e_wallet/view/widgets/buttons.dart';
+import 'package:e_wallet/view/widgets/forms.dart';
+import 'package:flutter/material.dart';
+
+class EditProfilePage extends StatelessWidget {
+  const EditProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            size: 24,
+          ),
+        ),
+        title: const Text(
+          'Update Profile',
+        ),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          const SizedBox(
+            height: 40,
+          ),
+          Container(
+            padding: const EdgeInsets.all(22),
+            decoration: BoxDecoration(
+              color: whiteColor,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                const CustomFormField(
+                  title: 'Username',
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const CustomFormField(
+                  title: 'Full Name',
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const CustomFormField(
+                  title: 'Email Address',
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const CustomFormField(
+                  title: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomFilledButton(
+                  title: 'Update Now',
+                  onPressed: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                        context, '/profile-edit-success', (route) => false);
+                  },
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
