9caa170c725b98cfbd577819b6206ee1831b2a75
Chandra <ardhyachandrax@gmail.com>
2024-01-25 22:56:48 +0700

    transfer result user item widget

---
+++ lib/view/widgets/transfer_result_user_item.dart
@@ -0,0 +1,94 @@
+import 'package:e_wallet/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class TransferResultUserItem extends StatelessWidget {
+  const TransferResultUserItem({
+    super.key,
+    required this.imageUrl,
+    required this.name,
+    required this.username,
+    this.isVerified = false,
+    this.isSelected = false,
+  });
+
+  final String imageUrl;
+  final String name;
+  final String username;
+  final bool isVerified;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 155,
+      height: 173,
+      padding: const EdgeInsets.symmetric(
+        vertical: 22,
+        horizontal: 14,
+      ),
+      decoration: BoxDecoration(
+        color: whiteColor,
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: isSelected ? blueColor : whiteColor,
+          width: 2,
+        ),
+      ),
+      child: Column(
+        children: [
+          Container(
+            width: 70,
+            height: 70,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(imageUrl),
+              ),
+            ),
+            child: isVerified
+                ? Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 19,
+                      height: 19,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: whiteColor,
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.check_circle,
+                          color: greenColor,
+                          size: 18,
+                        ),
+                      ),
+                    ),
+                  )
+                : null,
+          ),
+          const SizedBox(
+            height: 13,
+          ),
+          Text(
+            name,
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: medium,
+            ),
+          ),
+          const SizedBox(
+            height: 2,
+          ),
+          Text(
+            '@$username',
+            style: greyTextStyle.copyWith(
+              fontSize: 12,
+              fontWeight: regular,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
