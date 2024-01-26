7957d6b282cb41cff45eac923dc9157efaf851ec
Chandra <ardhyachandrax@gmail.com>
2024-01-26 15:31:40 +0700

    slicing data provider page

---
+++ lib/view/pages/data_provider_page/data_provider_page.dart
@@ -0,0 +1,119 @@
+import 'package:e_wallet/shared/theme.dart';
+import 'package:e_wallet/view/widgets/buttons.dart';
+import 'package:e_wallet/view/widgets/data_provider_item.dart';
+import 'package:flutter/material.dart';
+
+class DataProviderPage extends StatelessWidget {
+  const DataProviderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            size: 24,
+          ),
+        ),
+        title: const Text("Beli Data"),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Text(
+            'From Wallet',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semibold,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Image.asset(
+                'assets/img_wallet.png',
+                width: 80,
+                height: 55,
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '8008 2208 1996',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: medium,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    "Balance: Rp 180.000.000",
+                    style: greyTextStyle.copyWith(
+                      fontSize: 12,
+                      fontWeight: regular,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Text(
+            'Select Provider',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semibold,
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          const DataProviderItem(
+            imageUrl: 'assets/img_provider_telkomsel.png',
+            title: 'Telkomsel',
+            isSelected: true,
+          ),
+          const DataProviderItem(
+            imageUrl: 'assets/img_provider_indosat.png',
+            title: 'Indosat Ooredoo',
+          ),
+          const DataProviderItem(
+            imageUrl: 'assets/img_provider_singtel.png',
+            title: 'Singtel ID',
+          ),
+          const SizedBox(
+            height: 135,
+          ),
+          CustomFilledButton(
+            title: 'Continue',
+            onPressed: () {
+              Navigator.pushNamed(context, '/data-package');
+            },
+          ),
+          const SizedBox(
+            height: 57,
+          ),
+        ],
+      ),
+    );
+  }
+}
