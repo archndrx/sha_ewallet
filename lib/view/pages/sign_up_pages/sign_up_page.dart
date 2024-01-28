30ad0f2852df82050fe02e3122f9d4395b0ab48c
Chandra <ardhyachandrax@gmail.com>
2024-01-28 13:40:40 +0700

    add validator and use bloc consumer

--- lib/view/pages/sign_up_pages/sign_up_page.dart
+++ lib/view/pages/sign_up_pages/sign_up_page.dart
@@ -1,83 +1,153 @@
+import 'package:e_wallet/models/sign_up_model.dart';
+import 'package:e_wallet/shared/shared_methods.dart';
 import 'package:e_wallet/shared/theme.dart';
+import 'package:e_wallet/view/pages/sign_up_pages/sign_up_set_profile_page.dart';
 import 'package:e_wallet/view/widgets/buttons.dart';
 import 'package:e_wallet/view/widgets/forms.dart';
+import 'package:e_wallet/viewmodel/blocs/auth/auth_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final TextEditingController nameController = TextEditingController(text: '');
+  final TextEditingController emailController = TextEditingController(text: '');
+  final TextEditingController passwordController =
+      TextEditingController(text: '');
+
+  bool validate() {
+    if (nameController.text.isEmpty ||
+        passwordController.text.isEmpty ||
+        emailController.text.isEmpty) {
+      return false;
+    }
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24,
-        ),
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 100,
-              bottom: 100,
-            ),
-            child: Image.asset(
-              'assets/img_logo_light.png',
-              width: 155,
-              height: 50,
-            ),
-          ),
-          Text(
-            'Join Us to Unlock\nYour Growth',
-            style: blackTextStyle.copyWith(
-              fontSize: 20,
-              fontWeight: semibold,
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Container(
-            padding: const EdgeInsets.all(22),
-            decoration: BoxDecoration(
-              color: whiteColor,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const CustomFormField(title: 'Fullname'),
-                const SizedBox(
-                  height: 16,
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthFailed) {
+            showCustomSnackbar(context, state.e);
+          }
+
+          if (state is AuthCheckEmailSuccess) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SignUpSetProfile(
+                  data: SignUpModel(
+                    name: nameController.text,
+                    email: emailController.text,
+                    password: passwordController.text,
+                  ),
                 ),
-                const CustomFormField(title: 'Email Address'),
-                const SizedBox(
-                  height: 16,
-                ),
-                const CustomFormField(
-                  title: 'Password',
-                  obscureText: true,
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                CustomFilledButton(
-                  title: 'Continue',
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/sign-up-set-profile');
-                  },
-                ),
-              ],
+              ),
+            );
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
             ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          CustomTextButton(
-            title: 'Sign In',
-            onPressed: () {
-              Navigator.pushNamed(context, '/sign-in');
-            },
-          ),
-        ],
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 100,
+                  bottom: 100,
+                ),
+                child: Image.asset(
+                  'assets/img_logo_light.png',
+                  width: 155,
+                  height: 50,
+                ),
+              ),
+              Text(
+                'Join Us to Unlock\nYour Growth',
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: semibold,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                padding: const EdgeInsets.all(22),
+                decoration: BoxDecoration(
+                  color: whiteColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomFormField(
+                      title: 'Fullname',
+                      controller: nameController,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    CustomFormField(
+                      title: 'Email Address',
+                      controller: emailController,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    CustomFormField(
+                      title: 'Password',
+                      obscureText: true,
+                      controller: passwordController,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    CustomFilledButton(
+                      title: 'Continue',
+                      onPressed: () {
+                        if (validate()) {
+                          context.read<AuthBloc>().add(
+                                AuthCheckEmail(emailController.text),
+                              );
+                        } else {
+                          showCustomSnackbar(
+                              context, 'Semua field harus diisi');
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              CustomTextButton(
+                title: 'Sign In',
+                onPressed: () {
+                  Navigator.pushNamed(context, '/sign-in');
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
