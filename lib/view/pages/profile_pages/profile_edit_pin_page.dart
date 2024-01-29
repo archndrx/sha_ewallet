bfdb34e017b1355f964fbe92c49de28f654b8fba
Chandra <ardhyachandrax@gmail.com>
2024-01-29 21:58:11 +0700

    use bloc

--- lib/view/pages/profile_pages/profile_edit_pin_page.dart
+++ lib/view/pages/profile_pages/profile_edit_pin_page.dart
@@ -1,10 +1,23 @@
+import 'package:e_wallet/shared/shared_methods.dart';
 import 'package:e_wallet/shared/theme.dart';
 import 'package:e_wallet/view/widgets/buttons.dart';
 import 'package:e_wallet/view/widgets/forms.dart';
+import 'package:e_wallet/viewmodel/blocs/auth/auth_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ProfileEditPinPage extends StatelessWidget {
+class ProfileEditPinPage extends StatefulWidget {
   const ProfileEditPinPage({super.key});
+
+  @override
+  State<ProfileEditPinPage> createState() => _ProfileEditPinPageState();
+}
+
+class _ProfileEditPinPageState extends State<ProfileEditPinPage> {
+  final TextEditingController oldPinController =
+      TextEditingController(text: '');
+  final TextEditingController newPinController =
+      TextEditingController(text: '');
 
   @override
   Widget build(BuildContext context) {
@@ -24,45 +37,74 @@ class ProfileEditPinPage extends StatelessWidget {
           'Edit PIN',
         ),
       ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24,
-        ),
-        children: [
-          const SizedBox(
-            height: 40,
-          ),
-          Container(
-            padding: const EdgeInsets.all(22),
-            decoration: BoxDecoration(
-              color: whiteColor,
-              borderRadius: BorderRadius.circular(20),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthFailed) {
+            showCustomSnackbar(context, state.e);
+          }
+
+          if (state is AuthSuccess) {
+            Navigator.pushNamedAndRemoveUntil(
+                context, '/profile-edit-success', (route) => false);
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
             ),
-            child: Column(
-              children: [
-                const CustomFormField(
-                  title: 'Old PIN',
+            children: [
+              const SizedBox(
+                height: 40,
+              ),
+              Container(
+                padding: const EdgeInsets.all(22),
+                decoration: BoxDecoration(
+                  color: whiteColor,
+                  borderRadius: BorderRadius.circular(20),
                 ),
-                const SizedBox(
-                  height: 16,
+                child: Column(
+                  children: [
+                    CustomFormField(
+                      title: 'Old PIN',
+                      controller: oldPinController,
+                      keyType: TextInputType.number,
+                      obscureText: true,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    CustomFormField(
+                      title: 'New PIN',
+                      controller: newPinController,
+                      keyType: TextInputType.number,
+                      obscureText: true,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    CustomFilledButton(
+                      title: 'Update Now',
+                      onPressed: () {
+                        context.read<AuthBloc>().add(
+                              AuthUpdatePin(
+                                oldPinController.text,
+                                newPinController.text,
+                              ),
+                            );
+                      },
+                    ),
+                  ],
                 ),
-                const CustomFormField(
-                  title: 'New PIN',
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                CustomFilledButton(
-                  title: 'Update Now',
-                  onPressed: () {
-                    Navigator.pushNamedAndRemoveUntil(
-                        context, '/profile-edit-success', (route) => false);
-                  },
-                ),
-              ],
-            ),
-          ),
-        ],
+              ),
+            ],
+          );
+        },
       ),
     );
   }
