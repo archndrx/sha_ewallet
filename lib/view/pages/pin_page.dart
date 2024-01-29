5181eba1bdcbef3d2a7a7e46d2d0c5cc127fb717
Chandra <ardhyachandrax@gmail.com>
2024-01-29 21:15:32 +0700

    change pin to dynamic

--- lib/view/pages/pin_page.dart
+++ lib/view/pages/pin_page.dart
@@ -1,7 +1,9 @@
 import 'package:e_wallet/shared/shared_methods.dart';
 import 'package:e_wallet/shared/theme.dart';
 import 'package:e_wallet/view/widgets/buttons.dart';
+import 'package:e_wallet/viewmodel/blocs/auth/auth_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PinPage extends StatefulWidget {
   const PinPage({super.key});
@@ -12,6 +14,8 @@ class PinPage extends StatefulWidget {
 
 class _PinPageState extends State<PinPage> {
   final TextEditingController pinController = TextEditingController(text: '');
+  String pin = '';
+  bool isError = false;
 
   addPin(String number) {
     if (pinController.text.length < 6) {
@@ -21,21 +25,34 @@ class _PinPageState extends State<PinPage> {
     }
 
     if (pinController.text.length == 6) {
-      if (pinController.text == '123123') {
+      if (pinController.text == pin) {
         Navigator.pop(context, true);
       } else {
+        setState(() {
+          isError = true;
+        });
         showCustomSnackbar(
             context, 'PIN yang anda masukkan salah. Silakan coba lagi.');
       }
     }
   }
 
-  deletPin() {
+  deletePin() {
     if (pinController.text.isNotEmpty) {
       setState(() {
+        isError = false;
         pinController.text =
             pinController.text.substring(0, pinController.text.length - 1);
       });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    final authState = context.read<AuthBloc>().state;
+    if (authState is AuthSuccess) {
+      pin = authState.user.pin!;
     }
   }
 
@@ -72,6 +89,7 @@ class _PinPageState extends State<PinPage> {
                     fontSize: 36,
                     fontWeight: medium,
                     letterSpacing: 16,
+                    color: isError ? redColor : whiteColor,
                   ),
                   obscuringCharacter: '*',
                   decoration: InputDecoration(
@@ -154,7 +172,7 @@ class _PinPageState extends State<PinPage> {
                   ),
                   GestureDetector(
                     onTap: () {
-                      deletPin();
+                      deletePin();
                     },
                     child: Container(
                       width: 60,
